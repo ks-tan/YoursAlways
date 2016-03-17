85c9dc5d58a490f3ca3121214bdc1cf1537ddf00
kangsoontan <tankangsoon@gmail.com>
2016-03-17 07:56:22 -0700

    temporary hack to redirect users to static html pages

--- Routes/routesMain.js
+++ Routes/routesMain.js
@@ -3,8 +3,8 @@ var routes = express.Router();
 var rootPath = require('app-root-path');
 
 routes.get('/', function(req, res) {
-    res.sendFile(rootPath + '/public/views/main.html');
-    // res.render("TESTING HOME");
+    // res.sendFile(rootPath + '/public/views/main.html');
+    res.redirect('/views/main.html');
 });
 
 routes.get('/login', function(req, res) {
--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var routesMain = require('./Routes/routesMain');
 var app = express();
 app.set('port', (process.env.PORT || 5000));
 
+// In routesMain.js, replace res.redirect method with res.render.
+// Then, we may use 'ejs' templating engine.
+// app.set('view engine', 'ejs');
+
 app.use(express.static(__dirname + '/public'));
 app.use('/', routesMain);
 
