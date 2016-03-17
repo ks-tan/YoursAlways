df70861569c1e2bf8632eaee20ef6ce0745902f7
kangsoontan <tankangsoon@gmail.com>
2016-03-16 23:07:12 -0700

    change 'main' field in package.json to 'app.js'

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ var express = require('express');
 var routesMain = require('./Routes/routesMain');
 
 var app = express();
-var port = process.env.port || 9999;
+app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 app.use('/', routesMain);
 
-app.listen(port, function(){
+app.listen(app.get('port'), function(){
 	console.log("Server has started running....");
 });
