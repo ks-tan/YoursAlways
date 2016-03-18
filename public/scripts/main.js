85692645061fc055142b143deddea9f90bf9eb4b
kangsoontan <tankangsoon@gmail.com>
2016-03-17 23:27:47 -0700

    a bunch of styling for home page

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
 	$('.title_description').fadeIn("slow");
 	$('.title_main').fadeIn("slow", function(){
 		$('.title_button').fadeIn("slow");
+		$('.title_learn_more').fadeIn("slow");
 	});
 
 });
