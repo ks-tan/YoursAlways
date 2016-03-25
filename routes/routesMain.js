dd09268b428cd8add8bf4f63971bce4e98d4edb9
kangsoontan <tankangsoon@gmail.com>
2016-03-25 16:33:45 -0700

    add in routes folder again

---
+++ routes/routesMain.js
@@ -0,0 +1,63 @@
+var express = require('express');
+var routes = express.Router();
+var rootPath = require('app-root-path');
+var database = require(rootPath + '/common/databaseHelper');
+var Letter = require(rootPath + '/models/modelLetter');
+
+routes.get('/', function(req, res) {
+	res.render('pages/main');
+});
+
+routes.get('/write', function(req, res) {
+	res.render('pages/write', {letter: null});
+});
+
+routes.post('/newLetter', function(req, res) {
+	var letter = {
+		addressee: req.body.addressee,
+		letterBody: req.body.letterBody,
+		regards: req.body.regards,
+		addresser: req.body.addresser
+	};
+	database.saveObject(letter,Letter);
+	res.redirect('/read');
+});
+
+routes.get('/read', function(req,res){
+	database.findObject({},Letter,function(result){
+		res.render('pages/read', {letters: result});
+	});
+});
+
+routes.post('/deleteLetter', function(req,res){
+	var letter = {
+		_id: req.body._id
+	};
+	database.deleteObject(letter,Letter);
+	res.redirect('/read');
+});
+
+routes.post('/edit', function(req,res){
+	var letter = {
+		_id: req.body._id,
+	};
+	database.findObject(letter, Letter, function(result){
+		res.render('pages/write', {letter: result});
+	});
+});
+
+routes.post('/updateLetter', function(req,res){
+	var query = {
+		_id: req.body._id
+	};
+	var update = {
+		addressee: req.body.addressee,
+		letterBody: req.body.letterBody,
+		regards: req.body.regards,
+		addresser: req.body.addresser
+	};
+	database.updateObject(query,update,Letter);
+	res.redirect('/read');
+});
+
+module.exports = routes;
