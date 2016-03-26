15a17ea1f1faa353b25bf472f3dd88118ab2c180
kangsoontan <tankangsoon@gmail.com>
2016-03-25 17:40:50 -0700

    use javascript's date formatter instead

--- common/utilityHelper.js
+++ common/utilityHelper.js
@@ -1,15 +1,19 @@
 var utilityHelper = {
 
 	formatDate : function(currentDate){
-		var date = currentDate.getDate();
-		var monthIndex = currentDate.getMonth();
-		var month = ['','January','February','March','April','May','June',
-					'July','August','September','October','November','December'];
-		var year = currentDate.getFullYear();
-		var hours = currentDate.getHours() <= 12 ? currentDate.getHours() : currentDate.getHours()%12;
-		var timePostfix = currentDate.getHours() <= 12 ? 'am' : 'pm'	
-		var minutes = currentDate.getMinutes();
-		return date + ' ' + month[monthIndex] + ' ' + year + ', ' + hours + ':' + minutes + ' ' + timePostfix;
+		return currentDate.toLocaleDateString('en-US',
+			{ 
+				weekday: 'long',
+				year: 'numeric',
+				month: 'long',
+				day: 'numeric' 
+			})
+			+ ', '
+			+ currentDate.toLocaleTimeString('en-us',
+			{
+				hour: '2-digit',
+				minute:'2-digit'
+			});
 	}
 
 };
